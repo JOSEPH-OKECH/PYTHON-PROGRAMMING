63115247ffec6d5510e52300ea4ae7140230e970
JOSEPH-OKECH <137928254+JOSEPH-OKECH@users.noreply.github.com>
2023-06-27 21:30:13 +0300

    Add files via upload
    My first Python code.

---
+++ hello.py
@@ -0,0 +1,5 @@
+#My first Python code
+"""This is  a comment that i longer 
+than  1 line """
+print("Welcome to \n Python \n Programming")
+print("Love me \n love me \n You say \n that You \n love me\n")
